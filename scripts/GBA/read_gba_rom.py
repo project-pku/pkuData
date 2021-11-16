d495878a22709155cb69ad5efa8d09cabdcf888e
prof64 <80442784+prof64@users.noreply.github.com>
2021-11-16 18:33:09 -0500

    Add Base PP for all pk3 moves
    - Added the base PP of all moves that appear in pk3 (i.e. 1-354) as of Gen 8, in main-seriesMoveDex.
    -  Added the base PP of moves in Gen 3 that have a different PP from that of Gen 8, in pk3MoveDex.
    - Updated read_gba_rom script to scrape name and base pp data from moves.

--- scripts/GBA/read_gba_rom.py
+++ scripts/GBA/read_gba_rom.py
@@ -3,6 +3,7 @@ import commentjson
 import os
 import itertools as it
 
+# custom to_json method
 def to_json(o, level=0):
     INDENT = 2
     SPACE = " "
@@ -37,6 +38,56 @@ def to_json(o, level=0):
         raise TypeError("Unknown type '%s' for json serialization" % str(type(o)))
     return ret
 
+# compute index ranges
+def getIndices(indices_str):
+    index_ranges = indices_str.split(", ")
+    valid_indices = it.chain()
+    for r in index_ranges:
+        extrema = [int(x) for x in r.split('-')]
+        if('-' not in r):
+            valid_indices = it.chain(valid_indices, range(extrema[0], extrema[0]+1))
+        else:
+            valid_indices = it.chain(valid_indices, range(extrema[0], extrema[1]+1))
+    return valid_indices
+
+# pk3 char encoding -> unicode
+def encodeName(id, offset, length):
+    name = ""
+    for x in range(length):
+        char = char_enc[str(rom[offset + id*length + x])]
+        if(char == '\u0000'):
+            break
+        name += char_enc[str(rom[offset + id*length + x])]
+    return name
+
+# grab species data (name, ability slots)
+def getSpeciesData(profile_name):
+    output = {}
+    name_offset = profiles[profile_name]["Species Data"]["Names Offset"]
+    table_offset = profiles[profile_name]["Species Data"]["Table Offset"]
+    valid_indices = getIndices(profiles[profile_name]["Species Data"]["Valid Indices"])
+    for id in valid_indices:
+        species = {}
+        species["name"] = encodeName(id, name_offset, 11)
+        species["slot 1"] = rom[table_offset + id*28 + 22]
+        species["slot 2"] = rom[table_offset + id*28 + 23]
+        output[id] = species
+    return output
+
+# grab move data (name, base pp)
+def getMoveData(profile_name):
+    output = {}
+    name_offset = profiles[profile_name]["Move Data"]["Names Offset"]
+    table_offset = profiles[profile_name]["Move Data"]["Table Offset"]
+    valid_indices = getIndices(profiles[profile_name]["Move Data"]["Valid Indices"])
+    for id in valid_indices:
+        move = {}
+        move["name"] = encodeName(id, name_offset, 13)
+        move["base pp"] = rom[table_offset + id*12 + 4]
+        output[id] = move
+    return output
+
+
 # ----------------
 ## Begin Script
 # ----------------
@@ -51,31 +102,10 @@ profile_name = input("Enter the ROM profile to be used:\n")
 with open(os.path.join(root, profiles[profile_name]["ROM"]), 'rb') as f:
     rom = f.read()
 
-# compute index ranges
-index_ranges = profiles[profile_name]["Valid Indices"].split(", ")
-valid_indices = it.chain()
-for r in index_ranges:
-    extrema = [int(x) for x in r.split('-')]
-    if('-' not in r):
-        valid_indices = it.chain(valid_indices, range(extrema[0], extrema[0]+1))
-    else:
-        valid_indices = it.chain(valid_indices, range(extrema[0], extrema[1]+1))
-
 # generate output
 output = {}
-name_offset = profiles[profile_name]["Species Names Offset"]
-table_offset = profiles[profile_name]["Species Table Offset"]
-for id in valid_indices:
-    species = {}
-    species["name"] = ""
-    for x in range(11):
-        char = char_enc[str(rom[name_offset + id*11 + x])]
-        if(char == '\u0000'):
-            break
-        species["name"] += char_enc[str(rom[name_offset + id*11 + x])]
-    species["slot 1"] = rom[table_offset + id*28 + 22]
-    species["slot 2"] = rom[table_offset + id*28 + 23]
-    output[id] = species
+output["Species"] = getSpeciesData(profile_name)
+output["Moves"] = getMoveData(profile_name)
 
 # all done, write file
 with open(os.path.join(root,'out.json'), 'w', encoding='utf-8') as f:
