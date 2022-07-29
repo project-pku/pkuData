7c6ca55029c2c8cd7c58bf0bdf61cc216d57cf51
prof64 <80442784+prof64@users.noreply.github.com>
2022-07-29 06:08:53 -0400

    Add dex manifests
    Updated dex compilation system. Now each group of dexes has a manifest.json file that corresponds to it. These files map each dex type (species, item, etc.) to their location relative to the manifest.
    
    All of these manifests are then registered in the masterManifest.json in the root.
    
    The compile_masterdexes script was updated to reflect this.

--- scripts/Compile Masterdexes/compile_masterdexes.py
+++ scripts/Compile Masterdexes/compile_masterdexes.py
@@ -1,7 +1,8 @@
 # A python script that compiles collections of partial datadexes
 # into masterdexes as specified by dexlist.json
 import commentjson
-import os
+# import os
+import os.path
 
 # custom to_json method
 def to_json(o, level=0):
@@ -56,19 +57,34 @@ def merge_dicts(base, new):
 # ----------------
 ## Begin Script
 # ----------------
-
 # initialize
 root = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "..")
 masterdex_path = os.path.join(root, "masterdexes")
-dexlist = commentjson.load(open(os.path.join(root, "scripts", "Compile Masterdexes", "dexlist.json"), encoding="utf8"))
+masterManifest = commentjson.load(open(os.path.join(root, "masterManifest.json"), encoding="utf8"))
 
-for key in dexlist:
+# collect subdexes
+dexes = {}
+for _, manifestLoc in masterManifest["Sub-Manifests"].items():
+  manifestRoot = os.path.dirname(manifestLoc)
+  manifest = commentjson.load(open(os.path.join(root, manifestLoc), encoding="utf8"))
+  for dexType, dexLocOrLocs in manifest.items():
+    #cast single dex location to array of locations
+    dexLocs = [dexLocOrLocs] if type(dexLocOrLocs) == str else dexLocOrLocs
+
+    for dexLoc in dexLocs:
+      fullDexLoc = os.path.join(manifestRoot, dexLoc)
+      if dexType in dexes:
+        dexes[dexType].append(fullDexLoc)
+      else:
+        dexes[dexType] = [fullDexLoc]
+
+# process each dex type (in reverse order so that first dexes overwrite later ones)
+for dexType, dexList in dexes.items():
   # merge datadexes
   first = True
   base = None
-  # reverse order (so that earlier dexes have precedent i.e. override later ones.)
-  for x in reversed(dexlist[key]["SubDexes"]):
-    sdex = commentjson.load(open(os.path.join(root, dexlist[key]["SubDexes"][x]), encoding="utf8"))
+  for dexLoc in reversed(dexList):
+    sdex = commentjson.load(open(os.path.join(root, dexLoc), encoding="utf8"))
     if first:
       base = sdex
       first = False
@@ -76,8 +92,8 @@ for key in dexlist:
       merge_dicts(base, sdex)
 
   # merging complete, write file
-  with open(os.path.join(masterdex_path,'master{0}Dex.json'.format(key)), 'w', encoding='utf-8') as f:
+  with open(os.path.join(masterdex_path,'master{0}Dex.json'.format(dexType)), 'w', encoding='utf-8') as f:
     f.write(to_json(base))
-  print("Compiled master{0}Dex...".format(key))
+  print("Compiled master{0}Dex...".format(dexType))
 
 input("\nFinished compiling masterdexes!")
